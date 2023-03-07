a0d8d3d6a2bddc539cc8853be25e02927236c958
ADB-stark <115967404+ADB-stark@users.noreply.github.com>
2023-03-07 20:24:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    #    url = "https://api.themoviedb.org/3/movie/{}?api_key=2e5f549d447603cfbd2562fe364c1b1f&language=en-US".format(movie_id)
+
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
